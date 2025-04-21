a6cbff1f42686d8c9dd9058c53cb26b116570433
donphi <w2107579@westminster.ac.uk>
2025-04-21 08:22:34 +0100

    Added transparency in notch

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -46,9 +46,10 @@ export default function Document() {
                 {/* Minimal PWA metadata */}
                 <meta name="apple-mobile-web-app-title" content="Dodo by Biobankly" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
-                <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
                 <meta name="format-detection" content="telephone=no" />
                 <meta name="mobile-web-app-capable" content="yes" />
+                <meta name="theme-color" content="transparent" />
 
                 {/* SEO */}
                 <link rel="canonical" href="https://dodo.biobankly.com/" />
